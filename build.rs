b143da41ac66daffdb927d0ddb1bf9f1a845582f
Luke Davies <lkdvs3@gmail.com>
2019-10-10 22:44:54 +0100

    Attempted to set up build with libs

---
+++ build.rs
@@ -0,0 +1,13 @@
+use std::path::Path;
+use std::process::Command;
+
+fn main() {
+    let matrix_lib = Path::new("vendor/rpi-rgb-led-matrix");
+
+    Command::new("make")
+        .current_dir(&matrix_lib)
+        .status()
+        .unwrap();
+
+    println!(r"cargo:rustc-link-search=./vendor/rpi-rgb-led-matrix/lib");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use rpi_led_matrix::{LedMatrix, LedMatrixOptions};
+
 fn main() {
     println!("Hello, world!");
+
+    let mut options = LedMatrixOptions::new();
+    options.set_hardware_mapping("adafruit-hat");
+    options.set_chain_length(1);
+    options.set_hardware_pulsing(false);
+    //options.set_inverse_colors(true);
+    //options.set_refresh_rate(true);
+    LedMatrix::new(Some(options)).unwrap();
+
+    ();
 }
